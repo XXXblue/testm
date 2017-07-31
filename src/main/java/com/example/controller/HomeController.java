6e21412abc728120c5a4106359f67231a599afac
XXXblue <1123049661@qq.com>
2017-07-31 21:47:57 +0800

    我不要那个logger提示了

--- src/main/java/com/example/controller/HomeController.java
+++ src/main/java/com/example/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  *@Author: Xblue
- *@Description： 测试主页
+ *@Description; 测试
  *@Date: 20:32 2017/7/31
  */
 // 注解标注此类为springmvc的controller，url映射为"/home"
@@ -21,7 +21,7 @@ public class HomeController {
     @RequestMapping("/index")
     public  String index(){
         //输出日志文件
-        logger.info("the first jsp pages");
+//        logger.info("the first jsp pages");
         //返回一个index.jsp这个视图
         return "home";
     }
